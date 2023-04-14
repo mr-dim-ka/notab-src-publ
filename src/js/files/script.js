3d653330e9aba5eb733cee215390022ce729104f
mr-dim-ka <kalinin9.7@mail.ru>
2023-04-14 11:07:10 +0600

    added function addClassLoaded

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,14 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+
+function addClassLoaded() {
+	window.addEventListener("load", function () {
+		setTimeout(function () {
+			document.documentElement.classList.add('loaded');
+		}, 0);
+	});
+}
+
+addClassLoaded();
